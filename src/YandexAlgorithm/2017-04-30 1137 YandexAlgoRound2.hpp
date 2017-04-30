2a4fb37294160662e059d657c8427917d02fa1dd
Sergey Tyulenev <marleeeeeey@gmail.com>
2017-04-30 15:07:19 +0300

    add second resolve

--- src/YandexAlgorithm/2017-04-30 1137 YandexAlgoRound2.hpp
+++ src/YandexAlgorithm/2017-04-30 1137 YandexAlgoRound2.hpp
@@ -1,8 +1,7 @@
-#include "AllInclude.h"
-
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <list>
 
 
 int main()
@@ -10,7 +9,52 @@ int main()
     int n;
     std::cin >> n;
 
-    std::vector<int> tasks;
+    typedef std::vector<int> Tasks;
+    Tasks tasks;
+    tasks.reserve(n);
+
+    for (int i = 0; i < n; ++i)
+    {
+        int taskNumber;
+        std::cin >> taskNumber;
+        tasks.push_back(taskNumber);
+    }
+    
+    std::vector<Tasks> cycles;
+    
+
+    for (int i = 0; i < tasks.size(); ++i)
+    {
+        int curTask = tasks[i];
+
+        bool isPushBack = false;
+
+        for (auto & c : cycles)
+        {
+            if (c.back() == curTask - 1)
+            {
+                c.push_back(curTask);
+                isPushBack = true;
+                break;
+            }
+        }
+
+        if (!isPushBack)
+        {
+            cycles.push_back({ curTask });
+        }
+    }
+
+    std::cout << cycles.size() << std::endl;
+}
+
+
+int main_1()
+{
+    int n;
+    std::cin >> n;
+
+    std::list<int> tasks;
 
     for (int i = 0; i < n; ++i)
     {
@@ -21,24 +65,37 @@ int main()
 
     int searchTask = 1;
     int count = 1;
+    int lastTask = tasks.size();
 
-    auto it = std::find(tasks.begin(), tasks.end(), searchTask);
-    
+    typedef std::list<int>::iterator ItInt;
+
+    ItInt it = std::find(tasks.begin(), tasks.end(), searchTask);
+
+    std::vector<ItInt> removeItems;
 
     while (true)
     {
         if (it == tasks.end())
             break;
-        
+
+        removeItems.push_back(it);
+
         int nextTask = searchTask + 1;
 
-        if (nextTask > tasks.size())
+        if (nextTask > lastTask)
             break;
 
         auto itNext = std::find(it, tasks.end(), nextTask);
 
         if (itNext == tasks.end())
         {
+            for (auto & item : removeItems)
+            {
+                tasks.erase(item);
+            }
+
+            removeItems.clear();
+
             it = std::find(tasks.begin(), tasks.end(), nextTask);
             count++;
         }
@@ -51,4 +108,6 @@ int main()
     }
 
     std::cout << count << std::endl;
+
+    return 0;
 }
