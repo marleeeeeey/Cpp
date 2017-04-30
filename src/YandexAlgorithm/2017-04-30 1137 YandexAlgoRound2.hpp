e1f201e6c8821e10fbda051dc5bca43e5c609a26
Sergey Tyulenev <marleeeeeey@gmail.com>
2017-04-30 12:14:11 +0300

    add resolve from olimp

---
+++ src/YandexAlgorithm/2017-04-30 1137 YandexAlgoRound2.hpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    int n;
+    std::cin >> n;
+
+    std::vector<int> tasks;
+
+    for (int i = 0; i < n; ++i)
+    {
+        int taskNumber;
+        std::cin >> taskNumber;
+        tasks.push_back(taskNumber);
+    }
+
+    int searchTask = 1;
+    int count = 1;
+
+    auto it = std::find(tasks.begin(), tasks.end(), searchTask);
+
+    while (true)
+    {
+        if (it == tasks.end())
+            break;
+
+        int nextTask = searchTask + 1;
+
+        if (nextTask > tasks.size())
+            break;
+
+        auto itNext = std::find(it, tasks.end(), nextTask);
+
+        if (itNext == tasks.end())
+        {
+
+//             it = std::find(tasks.begin(), tasks.end(), nextTask);
+// 
+//             if (it == tasks.end())
+//                 break;
+
+            count++;
+        }
+        else
+        {
+            it = itNext;
+        }
+
+        searchTask = nextTask;
+    }
+
+    std::cout << count << std::endl;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,15 @@
 //#include "SiteHackerrank/VariableSizedArrays.hpp"       //MARLEEEEEEY TODO
 //#include "InterviewingQuestions/YaUniqueOne.hpp"        //MARLEEEEEEY TODO
 //#include "InterviewingQuestions/WaterInPuddels.hpp"
+
 //#include "YandexAlgorithm/YandexAlgo2017.hpp"
+#include "YandexAlgorithm/2017-04-30 1137 YandexAlgoRound2.hpp"
+
 
 //#include "SiteCodility/test_demo.hpp"
 //#include "SiteCodility/test01.hpp"
 //#include "SiteCodility/test02.hpp"
 //#include "SiteCodility/test03.hpp"
-// #include "SiteCodility/test04.hpp"
-#include "SiteCodility/test05.hpp"
-// #include "SiteCodility/test06.hpp"
-// #include "SiteCodility/test07.hpp"
-// #include "SiteCodility/test08.hpp"
-// #include "SiteCodility/test09.hpp"
-// #include "SiteCodility/test10.hpp"
+//#include "SiteCodility/test04.hpp"
+//#include "SiteCodility/test05.hpp"
+
