e67ecde11b607390ae6fd2af90990c191d146d44
Sergey Tyulenev <marleeeeeey@gmail.com>
2017-05-06 12:01:40 +0300

    add base

--- src/SockLib/TestSockLib.hpp
+++ src/SockLib/TestSockLib.hpp
@@ -1,22 +1,11 @@
-#ifndef TestSockLib_h__
-#define TestSockLib_h__
-
+#include "StdPlus/SockLibPlus.hpp"
 #include "StdPlus/StdPlus.h"
-#include "SockLib.h"
-
-
-int sockLibTest()
-{
-    AFUN;
-    return 0;
-}
-
 
 int main()
 {
-    sockLibTest();
+    AMSG("hello from TestSockLib");
+    stdplus::sl_init();
+    APAUSE;
+    stdplus::sl_term();
     APAUSE;
 }
-
-
-#endif // TestSockLib_h__
