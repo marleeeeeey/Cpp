136ac9374d0d14b66759b0976831b0b8bb724cd9
Sergey Tyulenev <marleeeeeey@gmail.com>
2017-05-29 14:17:37 +0300

    fix

--- src/2017-05-29 1330 StdFuncionTest/_mainStdFunc.hpp
+++ src/2017-05-29 1330 StdFuncionTest/_mainStdFunc.hpp
@@ -27,47 +27,63 @@ public:
 };
 
 
-//typedef double(*FuncPtr)(double, double);
-typedef std::function<double(double, double)> FuncPtr;
-
-//typedef double(Math::*ClassFuncPtr)(double, double);
-typedef std::function<double(double, double)> ClassFuncPtr;
+typedef double(*FuncPtr)(double, double);
+typedef double(Math::*ClassFuncPtr)(double, double);
+typedef std::function<double(double, double)> StdFuncPtr;
 
 int main(int argc, char ** argv)
 {
-    std::vector<FuncPtr> funcs
-    {
-        sum,
-        diff,
-    };
+    Math math;
 
     double a = 2;
     double b = 5;
     ARED(a);
     ARED(b);
 
+    // *** use global func ptr ***
+
+    std::vector<FuncPtr> funcs
+    {
+        sum,
+        diff,
+    };
 
     for (FuncPtr funPtr : funcs)
     {
         AVAR(funPtr(a, b));
     }
 
-    Math math;
+
+    // *** use member class func ptr ***
 
     std::vector<ClassFuncPtr> classFuncs
     {
-//         &Math::sum,
-//         &Math::diff,
+        &Math::sum,
+        &Math::diff,
 
-        std::bind(&Math::sum, &math, a, b),
-        std::bind(&Math::diff, &math, a, b),
     };
 
 
     for (ClassFuncPtr classFunPtr : classFuncs)
     {
-        //AVAR((math.*classFunPtr)(a, b));
-        AVAR(classFunPtr(a, b));
+        AVAR((math.*classFunPtr)(a, b));
+        //AVAR(classFunPtr(a, b));
+    }
+
+
+    // *** use std::function for both ***
+
+    std::vector<StdFuncPtr> stdFuncs
+    {
+        sum,
+        diff,
+        std::bind(&Math::sum, &math, a, b),
+        std::bind(&Math::diff, &math, a, b),
+    };
+
+    for (StdFuncPtr funPtr : stdFuncs)
+    {
+        AVAR(funPtr(a, b));
     }
 
     APAUSE;
