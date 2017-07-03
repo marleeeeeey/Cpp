ceb0feea9098fdc6625822a81bac5e1e39764c85
Sergey Tyulenev <marleeeeeey@gmail.com>
2017-07-04 01:38:36 +0300

    add settings and m_averageLineSize

--- src/2017-07-03 1604 CodeLiner/CodeLinerMain.hpp
+++ src/2017-07-03 1604 CodeLiner/CodeLinerMain.hpp
@@ -3,6 +3,18 @@
 #include "StdPlus/StdPlus.h"
 
 
+
+template <typename T>
+double getAverage(const std::vector<T> & d)
+{
+    if (d.empty())
+        return 0;
+
+    T sum = std::accumulate(d.begin(), d.end(), 0);
+    double average = (double)sum / d.size();
+    return average;
+}
+
 class FileLiner
 {
 public:
@@ -30,6 +42,8 @@ public:
                 continue;
             }
             
+            m_fillLinesSize.push_back(line.size());
+
             m_fillLineCounter++;
         }
 
@@ -39,6 +53,7 @@ public:
     inline int getEmptyLineCounter() const { return m_emptyLineCounter; }
     inline int getFillLineCounter() const { return m_fillLineCounter; }
     inline int getCommentLineCounter() const { return m_commentLineCounter; }
+    inline double getAverageLineSize() const { return getAverage(m_fillLinesSize); }
 
 private:
 
@@ -53,10 +68,11 @@ private:
             return false;
     }
 
-    std::string m_fileName;
-    int         m_emptyLineCounter   = 0;
-    int         m_fillLineCounter    = 0;
-    int         m_commentLineCounter = 0;
+    std::string      m_fileName;
+    int              m_emptyLineCounter   = 0;
+    int              m_fillLineCounter    = 0;
+    int              m_commentLineCounter = 0;
+    std::vector<int> m_fillLinesSize;
 
 };
 
@@ -65,70 +81,133 @@ std::ostream & operator<<(std::ostream & os, const FileLiner & d)
     std::string shortFileName = stdplus::fileNameFromFullPath(d.getFileName());
 
     os
-        << "empty: "    << std::setw(4)  << d.getEmptyLineCounter() << "; "
-        << "comment: "  << std::setw(4)  << d.getCommentLineCounter() << "; "
-        << "fill: "     << std::setw(4)  << d.getFillLineCounter() << "; "
+        << "empty: "    << std::setw(4)  << d.getEmptyLineCounter()     << "; "
+        << "comment: "  << std::setw(4)  << d.getCommentLineCounter()   << "; "
+        << "fill: "     << std::setw(4)  << d.getFillLineCounter()      << "; "
+        << "average: "  << std::setw(4)  << (int)d.getAverageLineSize() << "; "
         << "fileName: " /*<< std::setw(15)*/ << shortFileName << "; "
         ;
 
     return os;
 }
 
+class CodeLinerApplication
+{
+public:
+    CodeLinerApplication(int argc, char ** argv)
+    {
+        createReportFile();
+
+        if (argc > 1)
+            m_mainDir = argv[1];
+
+        AVAR(m_mainDir);
+        ASPLIT;
+
+        openSettingsFile();
+        readFileMasksFromSettings();
+        seachFileNames();
+        calculateResult();
+    }
+
+    ~CodeLinerApplication()
+    {
+        LOG_STD_UNREGISTER_STREAM(m_reportFile);
+        AMSG("You can see Report " + REPORT_FILE_NAME);
+        APAUSE_MSG("PRESS ANY KEY FOR CLOSE CONSOLE");
+    }
+
+private:
+
+    void calculateResult()
+    {
+        std::vector<FileLiner> fileLiners;
+
+        for (const std::string & fileName : m_fileNames)
+        {
+            fileLiners.emplace_back(fileName);
+        }
+
+        AVAR(fileLiners);
+        ASPLIT;
+
+        std::vector<double> averageLineSizes;
+        for (const auto & fileLiner : fileLiners)
+        {
+            m_sumEmpty += fileLiner.getEmptyLineCounter();
+            m_sumFill += fileLiner.getFillLineCounter();
+            m_sumComment += fileLiner.getCommentLineCounter();
+            averageLineSizes.push_back(fileLiner.getAverageLineSize());
+        }
+
+        m_averageLineSize = getAverage(averageLineSizes);
+
+        AVAR(m_sumEmpty);
+        AVAR(m_sumComment);
+        AVAR(m_sumFill);
+        AVAR(m_averageLineSize);
+        ASPLIT;
+    }
+
+    void seachFileNames()
+    {
+        AVAR(m_fileMasks);
+        ASPLIT;
+
+        m_fileNames = stdplus::getSubFilesIncludeSubfolders(m_mainDir, m_fileMasks);
+
+        AVAR(m_fileNames);
+        ASPLIT;
+    }
+
+    void readFileMasksFromSettings()
+    {
+        std::string fileMasksLine = m_settings.getValue<std::string>("fileMasks");
+        auto fileMasks = stdplus::split(fileMasksLine, ';');
+        m_fileMasks.insert(m_fileMasks.begin(), fileMasks.begin(), fileMasks.end());
+    }
+
+    void openSettingsFile()
+    {
+        try
+        {
+            m_settings.open(SETTINGS_FILE_NAME);
+        }
+        catch (std::logic_error &)
+        {
+            m_settings.setValue("fileMasks", "*.cpp;*.h;*.hpp;*.cs;*.c;");
+            m_settings.save(SETTINGS_FILE_NAME);
+        }
+    }
+
+    void createReportFile()
+    {
+        m_reportFile.open(REPORT_FILE_NAME, std::ofstream::out);
+        LOG_STD_REGISTER_STREAM(m_reportFile);
+
+        AMSG("developer: Tyulenev  Sergey <marleeeeeey@gmail.com>");
+        AMSG("all code: https://github.com/marleeeeeey/CppTests/blob/master/src/2017-07-03%201604%20CodeLiner/CodeLinerMain.hpp");
+        ASPLIT;
+    }
+
+
+    const std::string SETTINGS_FILE_NAME = "settigs.ini";
+    const std::string REPORT_FILE_NAME = stdplus::dateTimeStr() + " Report.txt";
+
+    stdplus::SimpleSettingsPlus m_settings;
+    std::ofstream               m_reportFile;
+    std::vector<std::string>    m_fileNames;
+    std::vector<std::string>    m_fileMasks; 
+    std::string                 m_mainDir = ".";
+
+    int m_sumEmpty = 0;
+    int m_sumFill = 0;
+    int m_sumComment = 0;
+    double m_averageLineSize = 0;
+
+};
+
 int main(int argc, char ** argv)
 {
-    std::string reportFileName = stdplus::dateTimeStr() + " Report.txt";
-
-    std::ofstream report(reportFileName);
-    LOG_STD_REGISTER_STREAM(report);
-
-    AMSG("developer: Tyulenev  Sergey <marleeeeeey@gmail.com>");
-    AMSG("all code: https://github.com/marleeeeeey/CppTests/blob/master/src/2017-07-03%201604%20CodeLiner/CodeLinerMain.hpp");
-    ASPLIT;
-
-    std::string dir = ".";
-    
-    if (argc > 1)
-        dir = argv[1];
-
-    AVAR(dir);
-    ASPLIT;
-
-    std::vector<std::string> fileMasks = { "*.cpp", "*.h", "*.hpp", "*.cs", "*.c" };
-
-    AVAR(fileMasks);
-    ASPLIT;
-
-    auto fileNames = stdplus::getSubFilesIncludeSubfolders(dir, fileMasks);
-
-    AVAR(fileNames);
-    ASPLIT;
-
-    std::vector<FileLiner> fileLiners;
-
-    for (const std::string & fileName : fileNames)
-    {
-        fileLiners.emplace_back(fileName);
-    }
-
-    AVAR(fileLiners);
-    ASPLIT;
-
-    int sumEmpty = 0;
-    int sumFill = 0;
-    int sumComment = 0;
-
-    for (const auto & fileLiner : fileLiners)
-    {
-        sumEmpty += fileLiner.getEmptyLineCounter();
-        sumFill += fileLiner.getFillLineCounter();
-        sumComment += fileLiner.getCommentLineCounter();
-    }
-
-    AVAR(sumEmpty);
-    AVAR(sumComment);
-    AVAR(sumFill);
-    ASPLIT;
-
-    AMSG("You can see Report " + reportFileName);
-    APAUSE_MSG("PRESS ANY KEY FOR CLOSE CONSOLE");
+    CodeLinerApplication app(argc, argv);
 }
