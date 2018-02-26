f88201a17d7924b4094a29f773806d408818b560
Sergey Tyulenev <marleeeeeey@gmail.com>
2018-02-26 23:27:34 +0300

    add win macros

---
+++ time/2018-02-26_2321_win_helper_print_debug/styulenev_win_helper.hpp
@@ -0,0 +1,42 @@
+#include <sstream>
+
+#define STYULENEV_OUT \
+    { \
+        std::wstringstream ss; \
+        ss << "styulenev: "
+#define STYULENEV_ENDL \
+            ; \
+        OutputDebugString(ss.str().c_str()); \
+    }
+#define STYULENEV_FUNC \
+    STYULENEV_OUT \
+        << "func=" << __FUNCTION__ \
+    STYULENEV_ENDL
+
+inline
+std::basic_ostream<wchar_t> & operator<<(std::basic_ostream<wchar_t>& os, const tagRECT& rect)
+{
+    os
+        << "("
+        << rect.left << ", "
+        << rect.top << ", "
+        << rect.right << ", "
+        << rect.bottom
+        << ")"
+        ;
+
+    return os;
+}
+
+inline 
+std::basic_ostream<wchar_t> & operator<<(std::basic_ostream<wchar_t>& os, const CPoint& point)
+{
+    os
+        << "("
+        << point.x << ", "
+        << point.y << ", "
+        << ")"
+        ;
+
+    return os;    
+}
