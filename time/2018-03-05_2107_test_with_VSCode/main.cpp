bcbbc401e524cbc1593bf4d84e105045ae679374
Sergey Tyulenev <marleeeeeey@gmail.com>
2018-03-05 23:14:03 +0300

    first compile with VSCode

---
+++ time/2018-03-05_2107_test_with_VSCode/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello from marleeeeeey" << std::endl;
+    int ret_value = 0;
+    std::cout << "input ret value: ";
+    std::cin >> ret_value;
+    return ret_value;
+}
