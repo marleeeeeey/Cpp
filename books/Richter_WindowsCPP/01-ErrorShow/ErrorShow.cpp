351014b013eb993bf3399478801a932ed345c6f6
Sergey Tyulenev <marleeeeeey@gmail.com>
2019-02-14 16:51:53 +0300

    apply Resharper code cleanup

--- books/Richter_WindowsCPP/01-ErrorShow/ErrorShow.cpp
+++ books/Richter_WindowsCPP/01-ErrorShow/ErrorShow.cpp
@@ -1,10 +1,4 @@
-/******************************************************************************
-Module:  ErrorShow.cpp
-Notices: Copyright (c) 2008 Jeffrey Richter & Christophe Nasarre
-******************************************************************************/
-
-
-#include "..\CommonFiles\CmnHdr.h"     /* See Appendix A. */
+#include "../CommonFiles/CmnHdr.h"     /* See Appendix A. */
 #include <Windowsx.h>
 #include <tchar.h>
 #include "Resource.h"
@@ -20,119 +14,129 @@ const TCHAR g_szAppName[] = TEXT("Error Show");
 ///////////////////////////////////////////////////////////////////////////////
 
 
-BOOL Dlg_OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam) {
+BOOL Dlg_OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam)
+{
+    chSETDLGICONS(hwnd, IDI_ERRORSHOW);
 
-   chSETDLGICONS(hwnd, IDI_ERRORSHOW);
+    // Don't accept error codes more than 5 digits long
+    Edit_LimitText(GetDlgItem(hwnd, IDC_ERRORCODE), 5);
 
-   // Don't accept error codes more than 5 digits long
-   Edit_LimitText(GetDlgItem(hwnd, IDC_ERRORCODE), 5);
-
-   // Look up the command-line passed error number
-   SendMessage(hwnd, ESM_POKECODEANDLOOKUP, lParam, 0);
-   return(TRUE);
+    // Look up the command-line passed error number
+    SendMessage(hwnd, ESM_POKECODEANDLOOKUP, lParam, 0);
+    return TRUE;
 }
 
 
 ///////////////////////////////////////////////////////////////////////////////
 
 
-void Dlg_OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify) {
+void Dlg_OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
+{
+    switch (id)
+    {
+    case IDCANCEL:
+        EndDialog(hwnd, id);
+        break;
 
-   switch (id) {
+    case IDC_ALWAYSONTOP:
+        SetWindowPos(hwnd, IsDlgButtonChecked(hwnd, IDC_ALWAYSONTOP)
+                               ? HWND_TOPMOST
+                               : HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+        break;
 
-   case IDCANCEL:
-      EndDialog(hwnd, id);
-      break;
+    case IDC_ERRORCODE:
+        EnableWindow(GetDlgItem(hwnd, IDOK), Edit_GetTextLength(hwndCtl) > 0);
+        break;
 
-   case IDC_ALWAYSONTOP:
-      SetWindowPos(hwnd, IsDlgButtonChecked(hwnd, IDC_ALWAYSONTOP) 
-         ? HWND_TOPMOST : HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
-      break;
+    case IDOK:
+        // Get the error code
+        DWORD dwError = GetDlgItemInt(hwnd, IDC_ERRORCODE, nullptr, FALSE);
 
-   case IDC_ERRORCODE: 
-      EnableWindow(GetDlgItem(hwnd, IDOK), Edit_GetTextLength(hwndCtl) > 0);
-      break;
+        HLOCAL hlocal = nullptr; // Buffer that gets the error message string
 
-   case IDOK:
-      // Get the error code
-      DWORD dwError = GetDlgItemInt(hwnd, IDC_ERRORCODE, NULL, FALSE);
+        // Use the default system locale since we look for Windows messages.
+        // Note: this MAKELANGID combination has 0 as value
+        DWORD systemLocale = MAKELANGID(LANG_NEUTRAL, SUBLANG_NEUTRAL);
 
-      HLOCAL hlocal = NULL;   // Buffer that gets the error message string
+        // Get the error code's textual description
+        BOOL fOk = FormatMessage(
+            FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS |
+            FORMAT_MESSAGE_ALLOCATE_BUFFER,
+            nullptr, dwError, systemLocale,
+            (PTSTR)&hlocal, 0, nullptr);
 
-      // Use the default system locale since we look for Windows messages.
-      // Note: this MAKELANGID combination has 0 as value
-      DWORD systemLocale = MAKELANGID(LANG_NEUTRAL, SUBLANG_NEUTRAL);
+        if (!fOk)
+        {
+            // Is it a network-related error?
+            HMODULE hDll = LoadLibraryEx(TEXT("netmsg.dll"), nullptr,
+                                         DONT_RESOLVE_DLL_REFERENCES);
 
-      // Get the error code's textual description
-      BOOL fOk = FormatMessage(
-         FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS |
-         FORMAT_MESSAGE_ALLOCATE_BUFFER, 
-         NULL, dwError, systemLocale, 
-         (PTSTR) &hlocal, 0, NULL);
+            if (hDll != nullptr)
+            {
+                fOk = FormatMessage(
+                    FORMAT_MESSAGE_FROM_HMODULE | FORMAT_MESSAGE_IGNORE_INSERTS |
+                    FORMAT_MESSAGE_ALLOCATE_BUFFER,
+                    hDll, dwError, systemLocale,
+                    (PTSTR)&hlocal, 0, nullptr);
+                FreeLibrary(hDll);
+            }
+        }
 
-      if (!fOk) {
-         // Is it a network-related error?
-         HMODULE hDll = LoadLibraryEx(TEXT("netmsg.dll"), NULL, 
-            DONT_RESOLVE_DLL_REFERENCES);
+        if (fOk && hlocal != nullptr)
+        {
+            SetDlgItemText(hwnd, IDC_ERRORTEXT, (PCTSTR)LocalLock(hlocal));
+            LocalFree(hlocal);
+        }
+        else
+        {
+            SetDlgItemText(hwnd, IDC_ERRORTEXT,
+                           TEXT("No text found for this error number."));
+        }
 
-         if (hDll != NULL) {
-            fOk = FormatMessage(
-               FORMAT_MESSAGE_FROM_HMODULE | FORMAT_MESSAGE_IGNORE_INSERTS |
-               FORMAT_MESSAGE_ALLOCATE_BUFFER,
-               hDll, dwError, systemLocale,
-               (PTSTR) &hlocal, 0, NULL);
-            FreeLibrary(hDll);
-         }
-      }
-
-      if (fOk && (hlocal != NULL)) {
-         SetDlgItemText(hwnd, IDC_ERRORTEXT, (PCTSTR) LocalLock(hlocal));
-         LocalFree(hlocal);
-      } else {
-         SetDlgItemText(hwnd, IDC_ERRORTEXT, 
-            TEXT("No text found for this error number."));
-      }
-
-      break;
-   }
+        break;
+    }
 }
 
 
 ///////////////////////////////////////////////////////////////////////////////
 
 
-INT_PTR WINAPI Dlg_Proc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+INT_PTR WINAPI Dlg_Proc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+    chHANDLE_DLGMSG(hwnd, WM_INITDIALOG, Dlg_OnInitDialog);
+    chHANDLE_DLGMSG(hwnd, WM_COMMAND, Dlg_OnCommand);
 
-   switch (uMsg) {
-      chHANDLE_DLGMSG(hwnd, WM_INITDIALOG, Dlg_OnInitDialog);
-      chHANDLE_DLGMSG(hwnd, WM_COMMAND,    Dlg_OnCommand);
-
-   case ESM_POKECODEANDLOOKUP:
-      SetDlgItemInt(hwnd, IDC_ERRORCODE, (UINT) wParam, FALSE);
-      FORWARD_WM_COMMAND(hwnd, IDOK, GetDlgItem(hwnd, IDOK), BN_CLICKED, 
+    case ESM_POKECODEANDLOOKUP:
+        SetDlgItemInt(hwnd, IDC_ERRORCODE, (UINT)wParam, FALSE);
+        FORWARD_WM_COMMAND(hwnd, IDOK, GetDlgItem(hwnd, IDOK), BN_CLICKED,
          PostMessage);
-      SetForegroundWindow(hwnd);
-      break;
-   }
+        SetForegroundWindow(hwnd);
+        break;
+    }
 
-   return(FALSE);
+    return FALSE;
 }
 
 
 ///////////////////////////////////////////////////////////////////////////////
 
 
-int WINAPI _tWinMain(HINSTANCE hinstExe, HINSTANCE, PTSTR pszCmdLine, int) {
-
-   HWND hwnd = FindWindow(TEXT("#32770"), TEXT("Error Show"));
-   if (IsWindow(hwnd)) {
-      // An instance is already running, activate it and send it the new #
-      SendMessage(hwnd, ESM_POKECODEANDLOOKUP, _ttoi(pszCmdLine), 0);
-   } else {
-      DialogBoxParam(hinstExe, MAKEINTRESOURCE(IDD_ERRORSHOW), 
-         NULL, Dlg_Proc, _ttoi(pszCmdLine));
-   }
-   return(0);
+int WINAPI _tWinMain(HINSTANCE hinstExe, HINSTANCE, PTSTR pszCmdLine, int)
+{
+    HWND hwnd = FindWindow(TEXT("#32770"), TEXT("Error Show"));
+    if (IsWindow(hwnd))
+    {
+        // An instance is already running, activate it and send it the new #
+        SendMessage(hwnd, ESM_POKECODEANDLOOKUP, _ttoi(pszCmdLine), 0);
+    }
+    else
+    {
+        DialogBoxParam(hinstExe, MAKEINTRESOURCE(IDD_ERRORSHOW),
+                       nullptr, Dlg_Proc, _ttoi(pszCmdLine));
+    }
+    return 0;
 }
 
 
