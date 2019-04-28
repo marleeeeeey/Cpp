8e6b556f84e804a6027310f6bffe77fdc7f4e338
Sergey Tyulenev <marleeeeeey@gmail.com>
2019-04-28 20:12:00 +0300

    add exception handling example

---
+++ time/2019-04-17_1204_Examples/return_lambda.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <iterator>
+#include <functional>
+
+auto get_lambda()
+{
+	int v = false;
+	
+	// in case v pass by value
+	// mutable used to make this variable non const.
+	auto lambdaFunc = [v](auto && b) mutable
+	{
+		b = v;
+		v = !v;
+	};
+
+	return lambdaFunc;
+}
+
+int mainReturnLambda()
+{
+	auto labmda = get_lambda();
+
+	std::vector<int> vec;
+	vec.resize(10);
+
+	std::for_each(vec.begin(), vec.end(), labmda);
+
+	std::cout << "vec : ";
+	std::copy(vec.begin(), vec.end(), std::ostream_iterator<int>(std::cout, " "));
+	std::cout << '\n';
+	std::cin.get();
+    return 0;
+}
