5edd43092d20158440566a6fc78961320c63e357
Sergey Tyulenev <marleeeeeey@gmail.com>
2019-11-14 22:49:33 +0300

    SFMLCollisionTestMain: code reformat

--- time/2019-11-14_2140_SFMLCollisionTest/SFMLCollisionTest/src/SFMLCollisionTestMain.cpp
+++ time/2019-11-14_2140_SFMLCollisionTest/SFMLCollisionTest/src/SFMLCollisionTestMain.cpp
@@ -11,7 +11,7 @@ sf::Font getDefaultFont()
 {
     sf::Font font;
     std::string fontFileName = "C:\\Windows\\Fonts\\calibri.ttf";
-    if(!font.loadFromFile(fontFileName))
+    if (!font.loadFromFile(fontFileName))
     {
         std::string msg = "Can't open font file " + fontFileName;
         throw std::exception(msg.c_str());
@@ -20,7 +20,7 @@ sf::Font getDefaultFont()
     return font;
 }
 
-sf::Text createDefaultTextObject(const std::string & msg = "")
+sf::Text createDefaultTextObject(const std::string& msg = "")
 {
     static sf::Font defaultFont = getDefaultFont();
 
@@ -31,14 +31,13 @@ sf::Text createDefaultTextObject(const std::string & msg = "")
     return text;
 }
 
-std::ostream & operator<<(std::ostream& os, const sf::FloatRect& rect)
+std::ostream& operator<<(std::ostream& os, const sf::FloatRect& rect)
 {
     os
         << rect.left << ", "
         << rect.top << ", "
         << rect.width << ", "
-        << rect.height
-        ;
+        << rect.height;
     return os;
 }
 
@@ -49,26 +48,26 @@ sf::RectangleShape createRectangleShape(sf::Vector2f size, sf::Vector2f pos)
     rect.setFillColor(sf::Color::Transparent);
     rect.setOutlineColor(sf::Color::White);
     rect.setOutlineThickness(2);
-    return  rect;
+    return rect;
 }
 
 void doMove(sf::Shape& moveObject, sf::Event event)
 {
-    if(isKeyPressed(event, sf::Keyboard::Key::Up))
+    if (isKeyPressed(event, sf::Keyboard::Key::Up))
     {
-        moveObject.move({ 0, -1 });
+        moveObject.move({0, -1});
     }
-    if(isKeyPressed(event, sf::Keyboard::Key::Down))
+    if (isKeyPressed(event, sf::Keyboard::Key::Down))
     {
-        moveObject.move({ 0, +1 });
+        moveObject.move({0, +1});
     }
-    if(isKeyPressed(event, sf::Keyboard::Key::Left))
+    if (isKeyPressed(event, sf::Keyboard::Key::Left))
     {
-        moveObject.move({ -1, 0 });
+        moveObject.move({-1, 0});
     }
-    if(isKeyPressed(event, sf::Keyboard::Key::Right))
+    if (isKeyPressed(event, sf::Keyboard::Key::Right))
     {
-        moveObject.move({ +1, 0 });
+        moveObject.move({+1, 0});
     }
 }
 
@@ -76,27 +75,26 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFMLCollisionTest");
     auto text1 = createDefaultTextObject();
-    auto rectangleShape1 = createRectangleShape({ 100, 100 }, { 400, 300 });
-    auto rectangleShape2 = createRectangleShape({ 100, 100 }, { 510, 300 });
-    
-    while(window.isOpen())
+    auto rectangleShape1 = createRectangleShape({100, 100}, {400, 300});
+    auto rectangleShape2 = createRectangleShape({100, 100}, {510, 300});
+
+    while (window.isOpen())
     {
         sf::Event event;
-        while(window.pollEvent(event))
+        while (window.pollEvent(event))
         {
-            if(event.type == sf::Event::Closed ||
-               isKeyPressed(event, sf::Keyboard::Key::Escape))
+            if (event.type == sf::Event::Closed ||
+                isKeyPressed(event, sf::Keyboard::Key::Escape))
             {
                 window.close();
             }
-            
+
             doMove(rectangleShape1, event);
         }
 
-
         const auto& rect1 = rectangleShape1.getGlobalBounds();
         const auto& rect2 = rectangleShape2.getGlobalBounds();
-        
+
         window.clear();
 
         text1.setString(std::to_string(rect1.intersects(rect2)));
