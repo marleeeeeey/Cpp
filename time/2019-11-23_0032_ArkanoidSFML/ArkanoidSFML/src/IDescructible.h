9b682bd04a4b6786401cd2e28423e014ae779afe
Sergey Tyulenev <marleeeeeey@gmail.com>
2019-12-03 00:41:12 +0300

    Arkanoid: minor fixes

--- time/2019-11-23_0032_ArkanoidSFML/ArkanoidSFML/src/IDescructible.h
+++ time/2019-11-23_0032_ArkanoidSFML/ArkanoidSFML/src/IDescructible.h
@@ -5,5 +5,6 @@ class IDescructible
 public:
     virtual ~IDescructible() = default;
     virtual void setLives(int lives) = 0;
+    //TODO remove from this interface
     virtual void setAppearance(int appearanceNumber) = 0;
 };
