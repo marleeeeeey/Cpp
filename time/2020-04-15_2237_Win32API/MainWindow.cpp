6e3f6154af7b05de37219c02504af9bc9f1bd080
Sergey Tyulenev <marleeeeeey@gmail.com>
2020-04-16 19:16:45 +0300

    Win32API: add custom ACCELERATORS

--- time/2020-04-15_2237_Win32API/MainWindow.cpp
+++ time/2020-04-15_2237_Win32API/MainWindow.cpp
@@ -1,13 +1,65 @@
 #include "MainWindow.h"
 #include <cassert>
 #include "ComLib.h"
+#include "resource.h"
 
 PCWSTR  MainWindow::ClassName() const { return L"Sample Window Class"; }
 
 LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+    wchar_t msg[32];
     switch (uMsg)
     {
+    case WM_COMMAND:
+        switch (LOWORD(wParam))
+        {
+        case ID_DRAW_MODE:
+            swprintf_s(msg, L"ID_DRAW_MODE\n");
+            OutputDebugString(msg);
+            break;
+
+        case ID_SELECT_MODE:
+            swprintf_s(msg, L"ID_SELECT_MODE\n");
+            OutputDebugString(msg);
+            break;
+
+        case ID_TOGGLE_MODE:
+            swprintf_s(msg, L"ID_TOGGLE_MODE\n");
+            OutputDebugString(msg);
+            break;
+        }
+        return 0;
+
+    case WM_SYSKEYDOWN:
+        swprintf_s(msg, L"WM_SYSKEYDOWN: 0x%x\n", wParam);
+        OutputDebugString(msg);
+        break;
+
+    case WM_SYSCHAR:
+        swprintf_s(msg, L"WM_SYSCHAR: %c\n", (wchar_t)wParam);
+        OutputDebugString(msg);
+        break;
+
+    case WM_SYSKEYUP:
+        swprintf_s(msg, L"WM_SYSKEYUP: 0x%x\n", wParam);
+        OutputDebugString(msg);
+        break;
+
+    case WM_KEYDOWN:
+        swprintf_s(msg, L"WM_KEYDOWN: 0x%x\n", wParam);
+        OutputDebugString(msg);
+        break;
+
+    case WM_KEYUP:
+        swprintf_s(msg, L"WM_KEYUP: 0x%x\n", wParam);
+        OutputDebugString(msg);
+        break;
+
+    case WM_CHAR:
+        swprintf_s(msg, L"WM_CHAR: %c\n", (wchar_t)wParam);
+        OutputDebugString(msg);
+        break;
+
     case WM_LBUTTONDOWN:
         OnLeftMouseClick();
         return DefWindowProc(m_hwnd, uMsg, wParam, lParam);
--- time/2020-04-15_2237_Win32API/main.cpp
+++ time/2020-04-15_2237_Win32API/main.cpp
@@ -1,9 +1,10 @@
 #include "WindowFactory.h"  
+#include "resource.h"
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
 {
     WindowFactory wf;
-    auto win = wf.Create("DrawingWindow");
+    auto win = wf.Create("MainWindow");
 
     if (!win->Create(L"Learn to Program Windows", WS_OVERLAPPEDWINDOW))
     {
@@ -12,13 +13,19 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow
 
     ShowWindow(win->Window(), nCmdShow);
 
+    HACCEL hAccel = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDR_ACCEL1));
+
+
     // Run the message loop.
 
     MSG msg = { };
     while (GetMessage(&msg, NULL, 0, 0))
     {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
+        if (!TranslateAccelerator(win->Window(), hAccel, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
     }
 
     return 0;
