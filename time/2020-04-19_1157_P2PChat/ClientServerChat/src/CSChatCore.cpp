9889f3065adb5bea7f36485fbc2ecda4f22e6a34
Sergey Tyulenev <marleeeeeey@gmail.com>
2020-04-28 21:48:37 +0300

    Chat: create empty ClientServerChat (UPD)

---
+++ time/2020-04-19_1157_P2PChat/ClientServerChat/src/CSChatCore.cpp
@@ -0,0 +1,13 @@
+#include "CSChatCore.h"
+
+CSChatCore::CSChatCore(IConnectionPointPtr cp, IUserInterfacePtr ui, ILoggerPtr logger)
+{
+}
+
+CSChatCore::~CSChatCore()
+{
+}
+
+void CSChatCore::start()
+{
+}
